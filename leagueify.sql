8faf43153aaaae8b929776418a2ea3d0dc12866c
Christian Long <christianzlong2@gmail.com>
2014-02-22 16:03:23 -0600

    MySQL format tables

---
+++ leagueify.sql
@@ -0,0 +1,13 @@
+drop table users;
+
+create table users
+( user_id varchar(256)
+, user_pass varchar(256)
+, email varchar(256)
+);
+
+insert into users values ('zo0o0ot', 'password', 'ross@leagueify.com');
+insert into users values ('matt', 'password', 'ross@leagueify.com');
+insert into users values ('zo0o0ot', 'password', 'ross@leagueify.com');
+
+
